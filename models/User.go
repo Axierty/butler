6644c1e0fa2f8980926cf2c1304043933d92efa6
zhenyupan <995336448@qq.com>
2019-06-21 17:59:12 +0800

    1

--- models/User.go
+++ models/User.go
@@ -13,3 +13,23 @@ func (User) Find(id int) User {
 	DB.Where("id = ?", id).Find(&result)
 	return result
 }
+
+func (User) UpdateById(id int,data map[string]interface{}) error {
+	var user User
+	DB.Find(&user, id)
+
+	//反射拿到user
+	//userReflect := reflect.ValueOf(user)
+	//
+	//for k, v := range data {
+	//
+	//	isExists := userReflect.FieldByName(k).Int()
+	//	if isExists == 0 {
+	//		return nil
+	//	}
+	//	user.k = v
+	//}
+	//db.Model(&user).Updates()
+	return nil
+
+}
