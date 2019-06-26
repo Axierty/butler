cb1ebb1e4979e78f560efd921eeb444b6901ed4c
zhenyupan <995336448@qq.com>
2019-06-26 10:56:26 +0800

    添加redis 配置

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# butler
+# go-study demo
 go学习写的练习web项目
+
+> 基于 gorm + gin
+
+```
+简单的目录划分
+
+├── configs
+├── controllers
+|   ├── BaseController
+├── middlewares
+├── models
+|   ├── Base.go
+├── resources
+|   ├── views
+├── routes
+|   └── api,go
+├── .env
+└── main.go
+```
--- configs/Base.go
+++ configs/Base.go
@@ -4,8 +4,10 @@ import "github.com/astaxie/beego/config"
 
 type Config struct {
 	DbCommonConfig
+	RedisCommonConfig
 }
 
+//数据库配置文件
 type DbCommonConfig struct {
 	DbHost     string
 	DbPort     string
@@ -15,29 +17,49 @@ type DbCommonConfig struct {
 	DbCharset  string
 }
 
+//redis配置文件
+type RedisCommonConfig struct {
+	RedisHost      string
+	RedisPort      string
+	RedisPass      string
+	RedisMaxIdle   string
+	RedisMaxActive string
+}
 
 var AppConfig *Config
 var DbConfig *DbCommonConfig
+var RedisConfig *RedisCommonConfig
 
 func init() {
 	AppConfig = &Config{}
 	DbConfig = &DbCommonConfig{}
 
 	//初始化文件
-	InitConfig(".env")
+	initConfig(".env")
 }
 
-
-func InitConfig(file string) {
+func initConfig(file string) {
 	conf, err := config.NewConfig("ini", file)
 	if err != nil {
 		panic(err)
 	}
+	initDbConfig(conf)
+	initRedisConfig(conf)
+}
+
+func initDbConfig(conf config.Configer) {
 	DbConfig.DbHost = conf.DefaultString("DB_HOST", "127.0.0.1")
 	DbConfig.DbPort = conf.DefaultString("DB_PORT", "8080")
 	DbConfig.DbDatabase = conf.DefaultString("DB_DATABASE", "test")
 	DbConfig.DbUsername = conf.DefaultString("DB_USERNAME", "root")
 	DbConfig.DbPassword = conf.DefaultString("DB_PASSWORD", "")
 	DbConfig.DbCharset = conf.DefaultString("DB_CHARSET", "utf8mb4")
+}
 
+func initRedisConfig(conf config.Configer) {
+	RedisConfig.RedisHost = conf.DefaultString("REDIS_HOST", "127.0.0.1")
+	RedisConfig.RedisPort = conf.DefaultString("REDIS_PORT", "6357")
+	RedisConfig.RedisPass = conf.DefaultString("REDIS_PASS", "")
+	RedisConfig.RedisMaxIdle = conf.DefaultString("REDIS_MAX_IDLE", "3")
+	RedisConfig.RedisMaxActive = conf.DefaultString("REDIS_MAX_ACTIVE", "5")
 }
